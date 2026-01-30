d132c401e1de4567b600c7efa9c8833962d23329
E <bobby@joe.com>
2026-01-30 01:13:58 -0500

    Changed from csv import to excel import for better compliance with the requirements.

--- Bulk-HybridUser.ps1
+++ Bulk-HybridUser.ps1
@@ -1,32 +1,35 @@
-﻿#On-premises section requirements: 
-#Run this from your Hybrid Exchange Server 2013 or 2016 (requires Exchange management shell)
-#Azure AD Connect should be present for the Start-ADSyncSyncCycle cmdlet to run
+﻿# On-premises section requirements: 
+# Run this from your Hybrid Exchange Server 2013 or 2016 (requires Exchange management shell)
+# Azure AD Connect should be present for the Start-ADSyncSyncCycle cmdlet to run
 
-#Add the Exchange PS Snap-in
+# Add the Exchange PS Snap-in
 write-host "Loading the PowerShell Snap-in for Exchange Management"
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 
-#Bulk-create hybrid users on-premises using CSV import
-write-host "Creating user(s) from CSV file"
-IMPORT-CSV GXPimport.csv | ForEach-Object { ./New-HybridUser.PS1 -TemplateUser $_.TemplateUser -UserAlias $_.UserAlias -DisplayName $_.DisplayName -FirstName $_.FirstName -LastName $_.LastName -UPN $_.UPN -Password (ConvertTo-SecureString -String $_.Password -AsPlainText -Force) -OU $_.OU }
+# Import the ImportExcel module
+write-host "Loading ImportExcel module"
+Import-Module ImportExcel
 
-#Force a sync of Azure AD Connect so the new users show up in the MSOL tenant
-#This command requires you to be on the server with Azure AD Connect installed
+# Bulk-create hybrid users on-premises using Excel import
+write-host "Creating user(s) from Excel file"
+Import-Excel GXPimport.xlsx | ForEach-Object { ./New-HybridUser.PS1 -TemplateUser $_.TemplateUser -UserAlias $_.UserAlias -DisplayName $_.DisplayName -FirstName $_.FirstName -LastName $_.LastName -UPN $_.UPN -Password (ConvertTo-SecureString -String $_.Password -AsPlainText -Force) -OU $_.OU }
+
+# Force a sync of Azure AD Connect so the new users show up in the MSOL tenant
+# This command requires you to be on the server with Azure AD Connect installed
 write-host "Running a delta sync from Azure AD Connect"
 Start-ADSyncSyncCycle -PolicyType Delta
 
 Start-Sleep -Seconds 60
 
-#Cloud section requirements: 
-#Install the Microsoft Graph PowerShell module
-#Requires scopes: User.ReadWrite.All, Directory.ReadWrite.All
+# Cloud section requirements: 
+# Install the Microsoft Graph PowerShell module
+# Requires scopes: User.ReadWrite.All, Directory.ReadWrite.All
 
-#Connect to Microsoft Graph
+# Connect to Microsoft Graph
 write-host "Connecting to Microsoft Graph"
 Import-Module Microsoft.Graph.Users
 Import-Module Microsoft.Graph.Users.Actions
 Connect-MgGraph -Scopes "User.ReadWrite.All", "Directory.ReadWrite.All" -NoWelcome
 
-#Bulk-assign licenses in Office 365 using CSV import
-write-host "Assigning user licenses and activating the mailboxes"
-IMPORT-CSV GXPimport.csv | ForEach-Object { ./Assign-License.PS1 -UPN $_.UPN -TemplateUser $_.TemplateUser }
+# Bulk-assign licenses in Office 365 using Excel import
+write-host "Assigning user licenses and activating the mailboxes"cel GXPimport.xlsx | ForEach-Object { ./Assign-License.PS1 -UPN $_.UPN -TemplateUser $_.TemplateUser }
